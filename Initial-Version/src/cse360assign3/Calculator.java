9e6e2879dfcfc2aed6cdeb3e7042a865b35824f7
YansongPeng <ypeng30@asu.edu>
2016-02-19 00:17:44 -0700

    Added files via upload

---
+++ Initial-Version/src/cse360assign3/Calculator.java
@@ -0,0 +1,61 @@
+package cse360assign3;
+
+/** Math calculation
+ * @author Yansong Peng
+ * @version Feb 18, 2016
+ * 
+ */
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** Calculate the total */
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Add a integer
+	 * 
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/** Subtract a value
+	 * 
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/** Multiply a value
+	 * 
+	 * @param value
+	 */
+	public void multiply (int value) {
+		
+	}
+	
+	/** Divide a value
+	 * 
+	 * @param value
+	 */
+	public void divide (int value) {
+		
+	}
+	
+	/** Display the result
+	 * 
+	 * @return	return the string will be printed
+	 */
+	public String getHistory () {
+		return "";
+	}
+}
